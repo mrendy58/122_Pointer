5bb4696177c931297eb5d3e68e97580d9de9fddd
mrendy58 <derendyyy58@gmail.com>
2023-05-08 11:26:46 +0700

    membuat kelas orang

---
+++ 122_friendKelas/122_friendKelas.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa;
+
+class orang {
+private:
+    string nama;
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
+class siswa
+int main()
+{
+    std::cout << "Hello World!\n";
+}
